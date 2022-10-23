e3b92764d3d4118507f2eb1f7c9af33dceb51f80
Jonah <jonahega@gmail.com>
2022-10-23 14:15:44 +0900

    Add MiniGolf assets

---
+++ Assets/MiniGolf/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// PLEASE NOTE! THIS SCRIPT IS FOR DEMO PURPOSES ONLY :) //
+
+public class Rotate : MonoBehaviour {
+	public GameObject rotationObject;
+	public int speed;
+
+	void Update () {
+		rotationObject.transform.Rotate (speed * Time.deltaTime, 0, 0);
+	}
+}
